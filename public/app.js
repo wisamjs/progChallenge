421fd696d5801fd0817a71bf37417462e05cd39d
wzaghal <wisamzaghal@gmail.com>
2014-07-12 10:44:57 -0400

    structural changes

--- public/app.js
+++ public/app.js
@@ -1,20 +1,33 @@
-angular.module('controllerAsExample', [])
-  .controller('MainController', MainController);
+var myApp = angular.module('MyApp', ['LocalStorageModule']);
 
-function MainController($scope, $timeout) {
+myApp.controller('MainController',
+	['$scope',
+	'$timeout',
+	'localStorageService',
+
+	function($scope,$timeout,localStorageService, genericServices) {
 	$scope.letter = 'o';
+	$scope.obj = { 'position':0, 'top':'50%','left':'0%'};
 
 	//the current string displayed onscreen
 	$scope.currString = $scope.letter;
 
+	//add extra letter
+	$scope.addLetter = function(){
 
-	$scope.onTimeout = function(){
-		//add extra letter
-    	$scope.currString+= $scope.letter;
-    	mytimeout = $timeout($scope.onTimeout,5000);
+    	$scope.currString += $scope.letter;
+    	$timeout($scope.addLetter,5000);
     }
-    
-    //call timeout function
-    var mytimeout = $timeout($scope.onTimeout,5000);
 
-}
+
+    //call timeout function
+    var mytimeout = $timeout($scope.addLetter,5000);
+}]);
+
+angular.module('myModule', ['LocalStorageModule'])
+.config(['localStorageServiceProvider', function(localStorageServiceProvider){
+  localStorageServiceProvider.setPrefix('newPrefix');
+}]);
+
+
+//timer function to add x and y values
